f028597f7b54c37fcb0cea6794cca0f965668509
Vithyashagar <vithyashagar98@gmail.com>
2021-04-16 02:07:14 +0530

    Insert Method added

--- src/main/java/Model/ManufacturerService.java
+++ src/main/java/Model/ManufacturerService.java
@@ -26,4 +26,46 @@ public class ManufacturerService {
 		}
 		return con;
 	}
+	
+public String insertService(String ServiceID, String Name, String Speciality, String Description, String MFRID) {
+		
+		String output = "";
+		
+		try {
+			
+			Connection con = connect();
+			
+			//Checking Connection
+			if(con == null) {
+				return "Error while connecting to Database";		
+			}
+			
+			//Create Prepared Statement
+			String query = "INSERT INTO services(`SID`,`ServiceID`,`Name`,`Speciality`,`Description`, `MFRID`) VALUES(?,?,?,?,?,?)" ;
+			
+			PreparedStatement preparedStmt  = con.prepareStatement(query);
+			
+			//Binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, ServiceID);
+			preparedStmt.setString(3, Name);
+			preparedStmt.setString(4, Speciality);
+			preparedStmt.setString(5, Description);
+			preparedStmt.setString(6, MFRID);
+			
+			//Execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Service Created Succesfully";
+			
+			
+		}catch(Exception e) {
+			output = "Error while Inserting Service";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
 }
