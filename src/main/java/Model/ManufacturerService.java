d5b938cce0365c410e485a68352e649d3a442021
Vithyashagar <vithyashagar98@gmail.com>
2021-04-16 02:07:52 +0530

    Read method created

--- src/main/java/Model/ManufacturerService.java
+++ src/main/java/Model/ManufacturerService.java
@@ -27,7 +27,7 @@ public class ManufacturerService {
 		return con;
 	}
 	
-public String insertService(String ServiceID, String Name, String Speciality, String Description, String MFRID) {
+	public String insertService(String ServiceID, String Name, String Speciality, String Description, String MFRID) {
 		
 		String output = "";
 		
@@ -67,5 +67,84 @@ public String insertService(String ServiceID, String Name, String Speciality, St
 		
 		return output;
 	}
+	
+	public String readServices()
+	{
+		String output = "";
+		try{
+			
+			Connection con = connect();
+			
+			//Checking Connection
+			if (con == null){
+				return "Error while Connecting to the Database for Reading.";
+			}
+			
+			// Prepare the HTML table to be displayed
+			output = "<table border='1'>"
+						+ "<tr>"
+							+ "<th>Name</th>"
+							+ "<th>Item Name</th>"
+							+ "<th>Speciality</th>"
+							+ "<th>Description</th>"
+							+ "<th>Manufacturer</th>"
+							+ "<th>Update</th>"
+							+ "<th>Remove</th>"
+						+ "</tr>";
+			
+			String query = "select * from services";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			// iterate through the rows in the result set
+			while (rs.next()){
+		
+				String SID = Integer.toString((rs.getInt("SID")));
+				String ServiceID = rs.getString("ServiceID");
+				String Name = rs.getString("Name");
+				String Speciality = rs.getString("Speciality");
+				String Description = rs.getString("Description");
+				String MFRID = rs.getString("MFRID");
+				
+				// Add a row into the HTML table
+				output += "<tr><td>" + ServiceID + "</td>";
+				output += "<td>" + Name + "</td>";
+				output += "<td>" + Speciality + "</td>";
+				output += "<td>" + Description + "</td>";
+				output += "<td>" + MFRID + "</td>";
+				
+				// buttons
+				output += "<td>"
+							+ "<form method='post' action='updateService.jsp'>"
+								+ "<input name='btnUpdate' type='submit' value='Update' class='btn btn-warning'>"
+								+ "<input name='SID' type='hidden' value=' " + SID + "'>"
+								+ "<input name='ServiceID' type='hidden' value=' " + ServiceID + "'>"
+								+ "<input name='Name' type='hidden' value=' " + Name + "'>"
+								+ "<input name='Speciality' type='hidden' value=' " + Speciality + "'>"
+								+ "<input name='Description' type='hidden' value=' " + Description + "'>"
+								+ "<input name='MFRID' type='hidden' value=' " + MFRID + "'>"
+							+ "</form></td>"
+						+ "<td>"
+							+ "<form method='post' action='Service.jsp'>"
+								+ "<input name='btnRemove' type='submit' value='Delete' class='btn btn-danger'>"
+								+ "<input name='itemID' type='hidden' value='" + SID + "'>"
+							+ "</form>"
+						+ "</td></tr>";
+				
+			}
+			
+			con.close();
+			// Complete the HTML table
+			output += "</table>";
+		}
+		catch (Exception e){
+			output = "Error while reading the Services.";
+			System.err.println(e.getMessage());
+			
+		}
+		
+		return output;
+	}
+
 	
 }
