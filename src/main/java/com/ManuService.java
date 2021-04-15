b18ec55764e37e26717bb3441d6dc33e22bc2c85
Vithyashagar <vithyashagar98@gmail.com>
2021-04-16 02:11:24 +0530

    Imported Necessary Files

---
+++ src/main/java/com/ManuService.java
@@ -0,0 +1,20 @@
+package com;
+
+import Model.ManufacturerService;
+
+//For Rest Services
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Service")
+public class ManuService {
+	
+}
