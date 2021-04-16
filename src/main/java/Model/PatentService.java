1689d46c204a1cb5111005a24bdf3e710c0973c2
Vithyashagar <vithyashagar98@gmail.com>
2021-04-17 00:21:56 +0530

    Patent Inserted method created

--- src/main/java/Model/PatentService.java
+++ src/main/java/Model/PatentService.java
@@ -26,6 +26,47 @@ public class PatentService {
 		return con;
 	}
 	
+	public String insertPatent(String PatentID, String Title, String appDate, String ResearcherID, String ConceptID) {
+	
+		String output = "";
+		
+		try {
+			
+			Connection con = connect();
+			
+			//Checking Connection
+			if(con == null) {
+				return "Error while connecting to Database";		
+			}
+			
+			//Create Prepared Statement
+			String query = "INSERT INTO patentapplication(`PID`,`PatentID`,`Title`,`appDate`,`ResearcherID`, `ConceptID`) VALUES(?,?,?,?,?,?)";
+			
+			PreparedStatement preparedStmt  = con.prepareStatement(query);
+			
+			//Binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, PatentID);
+			preparedStmt.setString(3, Title);
+			preparedStmt.setString(4, appDate);
+			preparedStmt.setString(5, ResearcherID);
+			preparedStmt.setString(6, ConceptID);
+			
+			//Execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Patent Form Created Succesfully";
+			
+			
+		}catch(Exception e) {
+			output = "Error while Inserting Service";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
 	
 	
 }
