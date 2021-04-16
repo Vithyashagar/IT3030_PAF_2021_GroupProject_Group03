fa3ca1b9b7f340d36f8c37342cbeca4bb488d1a0
Vithyashagar <vithyashagar98@gmail.com>
2021-04-17 00:16:02 +0530

    DB Connection created

--- src/main/java/Model/PatentService.java
+++ src/main/java/Model/PatentService.java
@@ -1,5 +1,31 @@
 package Model;
 
-public class PatentService {
+import java.sql.*;
 
+public class PatentService {
+	
+	public Connection connect(){
+		
+		Connection con = null;
+		
+		try{
+				
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//Connection
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/patentservices","root", "M@ng@th@9093");
+
+			//For Testing
+			System.out.println("Successfully connected");
+			
+		}
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	
+	
 }
