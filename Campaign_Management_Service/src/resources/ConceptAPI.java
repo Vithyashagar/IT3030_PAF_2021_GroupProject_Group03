36fa9190fba1721aafff782c6039fd6371f7ec75
Mariyam73 <shahimaria73@gmail.com>
2021-04-17 20:15:35 +0530

    Insert function complete

--- Campaign_Management_Service/src/resources/ConceptAPI.java
+++ Campaign_Management_Service/src/resources/ConceptAPI.java
@@ -24,5 +24,66 @@ public class ConceptAPI {
 		}
 		return con;
 	}
+	
+	
+	
+	
+	/******************************************* INSERT CONCEPT ****************************************************/
+	public String insertConcept(String conceptName, String conceptDesc, String startDate, String deadline, String pledgeGoal, String reward,String workUpdt, String researcherID, String manufactID){
+		
+		String output = "";
+		
+		try
+		{
+			Connection con = connect();
+			
+			if (con == null)
+			{
+				return "Database Connection failed!!";
+			}
+			
+			
+			//Preparing a CallableStatement to call a function
+		    CallableStatement cstmt = con.prepareCall("{? = call getCustomID()}");
+		    //Registering the out parameter of the function (return type)
+		    cstmt.registerOutParameter(1, Types.CHAR);
+		    //Executing the statement
+		    cstmt.execute();
+		    String conceptCode = cstmt.getString(1);
+			
+			// create a prepared statement
+			String query = " insert into concept(`conceptID`,`conceptCode`,`conceptName`,`conceptDesc`,`startDate`,`deadline`,`pledgeGoal`,`reward`,`workUpdt`,`researcherID`,`manufactID`)"
+			+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, conceptCode);
+			preparedStmt.setString(3, conceptName);
+			preparedStmt.setString(4, conceptDesc);
+			preparedStmt.setString(5, startDate);
+			preparedStmt.setString(6, deadline);
+			preparedStmt.setDouble(7, Double.parseDouble(pledgeGoal));
+			preparedStmt.setString(8, reward);
+			preparedStmt.setString(9, workUpdt);
+			preparedStmt.setString(10, researcherID);
+			preparedStmt.setString(11, manufactID);
+			
+			//execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Inserted successfully";
+		}
+		catch (Exception e)
+		{
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
 
 }
--- Campaign_Management_Service/src/service/ConceptService.java
+++ Campaign_Management_Service/src/service/ConceptService.java
@@ -17,5 +17,27 @@ import org.jsoup.nodes.Document;
 
 @Path("/Concepts")
 public class ConceptService {
+	
+	//concept API type object
+	ConceptAPI conceptObj = new ConceptAPI();
+	
+	//Insert Function
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertConcept(@FormParam("conceptName") String conceptName,
+							 @FormParam("conceptDesc") String conceptDesc,
+						     @FormParam("startDate") String startDate,
+						     @FormParam("deadline") String deadline,
+						     @FormParam("pledgeGoal") String pledgeGoal,
+						     @FormParam("reward") String reward,
+						     @FormParam("workUpdt") String workUpdt,
+						     @FormParam("researcherID") String researcherID,
+						     @FormParam("manufactID") String manufactID)
+	{
+		String output = conceptObj.insertConcept(conceptName, conceptDesc, startDate, deadline, pledgeGoal, reward, workUpdt, researcherID,manufactID );
+		return output;
+	}
 
 }
