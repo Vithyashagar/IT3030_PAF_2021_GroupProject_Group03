ce2bf7d4fe3aebe1ada46466347af855b30f02b3
HillaryJR <hillaryjeramin@gmail.com>
2021-04-17 18:31:32 +0530

    Added complete insertProduct() method

--- ProductService/src/main/java/model/Product.java
+++ ProductService/src/main/java/model/Product.java
@@ -28,5 +28,52 @@ public class Product {
 	 } 
 
 	
+	
+	
+	
+	/**********************Insert Product*****************************/
+	public String insertProduct(String productCode, String productName, String productPrice, String productDesc,String productCat){
+		String output = "";
+		
+	 try{
+		 Connection con = connect();
+	
+		 if (con == null)
+		 {
+			 return "Error while connecting to the database for inserting."; 
+		 }
+	 
+		 	// create a prepared statement
+		 	String query = " insert into product (`productId`,`productCode`,`productName`,`productPrice`,`productDesc`,`productCat`)"
+		 			+ " values (?, ?, ?, ?, ?, ?)";
+	
+		 	PreparedStatement preparedStmt = con.prepareStatement(query);
+		
+		 	// binding values
+		 	preparedStmt.setInt(1, 0);
+		 	preparedStmt.setString(2, productCode);
+		 	preparedStmt.setString(3, productName);
+		 	preparedStmt.setDouble(4, Double.parseDouble(productPrice));
+		 	preparedStmt.setString(5, productDesc);
+		 	preparedStmt.setString(6, productCat);
+		 	
+		 	// execute the statement
+		 	preparedStmt.execute();
+		 	con.close();
+		 	output = "Inserted successfully";
+		 	 
+	 }catch (Exception e){
+		 
+		 	 output = "Error while inserting the item.";
+		 	 System.err.println(e.getMessage());
+		 	}
+		 	
+	 	return output;
+	} 
+	
+
+	
+	
+	
 
 }
