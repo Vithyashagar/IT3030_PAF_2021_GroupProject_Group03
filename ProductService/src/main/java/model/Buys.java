f58baeebf75092e24d05f703652570f4a34d6da2
HillaryJR <hillaryjeramin@gmail.com>
2021-04-18 21:32:42 +0530

    Completed DB connect method in Buys.java

--- ProductService/src/main/java/model/Buys.java
+++ ProductService/src/main/java/model/Buys.java
@@ -1,5 +1,33 @@
 package model;
 
+import java.sql.*; 
+
 public class Buys {
+	
+	
+	
+	/**********************DB Connect*****************************/
+	//A common method to connect to the DB
+	@SuppressWarnings("unused")
+	private Connection connect()
+	 {
+		Connection con = null;
+	 try
+	 {
+		 Class.forName("com.mysql.jdbc.Driver");
+
+		 //Provide the correct details: DBServer/DBName, username, password
+		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/productservice", "root", "");
+	 
+	 }
+	 catch (Exception e)
+	 {
+		 	e.printStackTrace();
+		 	}
+	 
+	 	return con;
+	 } 
+	
+	
 
 }
