a3c3d54185bb9b75ec1109103fe4274e0429482a
HillaryJR <hillaryjeramin@gmail.com>
2021-04-20 23:21:42 +0530

    ProductSecurity DB bytesToHash completed

--- ProductService/src/main/java/security/ProductSecurity.java
+++ ProductService/src/main/java/security/ProductSecurity.java
@@ -7,5 +7,24 @@ import java.security.NoSuchAlgorithmException;
 public class ProductSecurity {
 	
 	
+	//Converting bytes to hex
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
+		public static String bytesToHex(byte[] bytes) {
+		  char[] hexChars = new char[bytes.length * 2];
+		  int v;
+		  for (int j = 0; j < bytes.length; j++) {
+		    v = bytes[j] & 0xFF;
+		    hexChars[j * 2] = hexArray[v >>> 4];
+		    hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		  }
+		  return new String(hexChars);
+		}
+		
+		
+		// Change this to encryoted form
+		private static String SALT = "P@7en79$#8*?";
+		
+		
+		
 }
