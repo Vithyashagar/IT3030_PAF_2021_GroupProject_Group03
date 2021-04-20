a43378f3f3a27529a82c91062202cfb68e40bf8d
Mariyam73 <shahimaria73@gmail.com>
2021-04-20 17:12:50 +0530

    Insert pledges complete

--- Campaign_Management_Service/src/resources/PledgeAPI.java
+++ Campaign_Management_Service/src/resources/PledgeAPI.java
@@ -1,7 +1,48 @@
 package resources;
 
 import java.sql.*;
+import util.DBConnection;
 
 public class PledgeAPI {
+	
+	//DBConnection object to establish connection
+	DBConnection dbConnect = new DBConnection();
+	
+	// -- Method to insert pledge --
+	public String insertPledge(String conceptID, String backerID, String pledgedAmnt){
+		
+		String output = "";
+		try
+		{
+			Connection con = dbConnect.connect();
+			if (con == null)
+			{
+				return "Database Connection failed!!";
+			}
+			
+			
+			// create a prepared statement
+			String query = " insert into backs(`conceptID`,`backerID`,`pledgedAmnt`)"
+			+ " values (?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(conceptID));
+			preparedStmt.setInt(2, Integer.parseInt(backerID));
+			preparedStmt.setDouble(3, Double.parseDouble(pledgedAmnt));
+			
+			//execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "You pledged the project successfully!!";
+		}
+		catch (Exception e)
+		{
+			output = "Failed to pledge!!";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 
 }
--- Campaign_Management_Service/src/service/PledgeService.java
+++ Campaign_Management_Service/src/service/PledgeService.java
@@ -19,5 +19,22 @@ import org.jsoup.nodes.Document;
 //Start of service
 @Path("/Pledges")
 public class PledgeService {
+	
+	
+	PledgeAPI pledgeObj = new PledgeAPI();
+	
+	
+	/** Inserting Pledge Amount to Backs table **/
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertConcept(@FormParam("conceptID") String conceptID,
+							 @FormParam("backerID") String backerID,
+							 @FormParam("pledgedAmnt") String pledgedAmnt)
+	{
+		String output = pledgeObj.insertPledge(conceptID,backerID,pledgedAmnt);
+		return output;
+	}
 
 }
