2e193771c8eceafc84793b4d036e828697b28f82
Vithyashagar <vithyashagar98@gmail.com>
2021-04-24 03:47:44 +0530

    Inter service communication added and Errors fixed

--- src/main/java/com/Patent.java
+++ src/main/java/com/Patent.java
@@ -5,9 +5,13 @@ import Model.PatentService;
 //For Rest Services
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 //For JSON
 import com.google.gson.*;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 
 @Path("/Patent")
 public class Patent {
@@ -26,15 +30,14 @@ public class Patent {
 		
 	//Method to Insert the Patent Application Info
 	@POST
-	@Path("/")
+	@Path("/{uName}")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_HTML)
-	public String insertPatent( @FormParam("Title") String Title,
-								@FormParam("appDate") String appDate,
-								@FormParam("ResearcherID") String ResearcherID,
-								@FormParam("ConceptID") String ConceptID) {
+	public String insertPatent( @PathParam("uName") String uName,
+								@FormParam("Title") String Title,
+								@FormParam("appDate") String appDate) {
 		
-		String output = PS.insertPatent(Title, appDate, ResearcherID, ConceptID);
+		String output = PS.insertPatent(Title, appDate, uName);
 		
 		return output;
 	}
@@ -57,6 +60,86 @@ public class Patent {
 		
 		return output;
 	}
-	//create methods for specific requirements	
+	
+	//Read conceptDescripion from Campaign Management
+	@GET
+	@Path("/ConceptDesc/{conceptName}")
+	public String  getConceptDescription(@PathParam("conceptName") String conceptName){
+		//Server path
+		String path = "http://localhost:8080/Campaign_Management_Service/ConceptService/Concepts/conceptDescription/";
 		
+		//create a client
+		Client client = Client.create();
+		WebResource target = client.resource(path);
+		
+		//retrieve details from server
+		String conceptDesc = target.queryParam("conceptName", conceptName).accept("application/xml").get(String.class);
+		
+		//return resource
+		return conceptDesc;
+		
+		//return response
+		//ClientResponse response = target.queryParam("conceptName", conceptName).accept("application/xml").get(ClientResponse.class);
+		 
+		// return response;
+	}
+	
+	//Read conceptDescripion from Campaign Management
+	@GET
+	@Path("/ConceptID/{conceptName}")
+	public String  getConceptID(@PathParam("conceptName") String conceptName){
+		//Server path
+		String path = "http://localhost:8080/Campaign_Management_Service/ConceptService/Concepts/conceptID/";
+		
+		//create a client
+		Client client = Client.create();
+		WebResource target = client.resource(path);
+		
+		//retrieve details from server
+		String conceptID = target.queryParam("conceptName", conceptName).accept("application/xml").get(String.class);
+		
+		//return resource
+		return conceptID;
+		
+		//return response
+		//ClientResponse response = target.queryParam("conceptName", conceptName).accept("application/xml").get(ClientResponse.class);
+		 
+		// return response;
+	}
+	
+	//Get Address by passing name
+	@GET
+	@Path("/Reasearcher/{Uname}")
+	@Produces(MediaType.TEXT_HTML)
+	public String getManufacturerAddress(@PathParam("Uname") String Uname){
+		
+		//Path to get the Manufacturer Name
+		String path = "http://localhost:8080/gadget_badget/UserService/Users/address/";
+	       
+	    //Create a client in Server to act as a client to another Server
+        Client client = Client.create();
+        
+        //Creating the web resource
+        WebResource target = client.resource(path);
+       
+        //To check the response if working
+        //ClientResponse response = target.queryParam("ID", Uname).accept("application/xml").get(ClientResponse.class);
+       
+        //Get the response String and save to a String(Response is a userID)
+        String response = target.queryParam("ID", Uname).accept("application/xml").get(String.class);
+        
+        //According to the userID get Manufacturer details
+    	return response.toString();	
+	}
+
+	//Method to Read the Patent Applications for a specific user
+		@GET
+		@Path("/{name}/")
+		@Produces(MediaType.TEXT_HTML)
+		public String readSPatents(@PathParam("name") String name) {
+			
+			return PS.readSPatent(name);
+		}
+	
+	
 }
